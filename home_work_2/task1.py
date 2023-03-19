234549dff50822d80999947657484706feb95b82
Polina <ms.maylin@inbox.ru>
2023-03-19 23:39:42 +0300

    Another solution added in task1

--- home_work_2/task1.py
+++ home_work_2/task1.py
@@ -1,18 +1,34 @@
 # Задача 10: На столе лежат n монеток. Некоторые из них лежат вверх решкой, а некоторые – гербом.
 # Определите минимальное число монеток, которые нужно перевернуть, чтобы все монетки были повернуты вверх одной и той же стороной.
 # Выведите минимальное количество монет, которые нужно перевернуть
-class NewEx(Exception):
-    pass
+# class NewEx(Exception):
+#     pass
+#
+#
+# try:
+#     coin_number = int(input("Введите количество монеток: "))
+#     tails = int(input("Введите количество монеток, лежащих вверх решкой: "))
+#     coat_of_arms = coin_number - tails
+#     if coin_number < tails:
+#         raise NewEx()
+#     print(f"Нужно перевернуть {coat_of_arms} монеток(ки).") if tails >= coat_of_arms else print(f"Нужно перевернуть {tails} монеток(ки).")
+# except ValueError:
+#     print("Введено некорректное значение.")
+# except NewEx:
+#     print("Количество монеток, лежащих решкой вверх, не может быть больше общего количества монеток.")
 
+# Решение через цикл
 
-try:
-    coin_number = int(input("Введите количество монеток: "))
-    tails = int(input("Введите количество монеток, лежащих вверх решкой: "))
-    coat_of_arms = coin_number - tails
-    if coin_number < tails:
-        raise NewEx()
-    print(f"Нужно перевернуть {coat_of_arms} монеток(ки).") if tails >= coat_of_arms else print(f"Нужно перевернуть {tails} монеток(ки).")
-except ValueError:
-    print("Введено некорректное значение.")
-except NewEx:
-    print("Количество монеток, лежащих решкой вверх, не может быть больше общего количества монеток.")
+n = int(input("Введите количество монеток: "))
+count_zero = 0
+count_one = 0
+for i in range(n):
+    x = int(input("Какой стороной лежит следующая монетка? (0 - решка, 1 - герб): "))
+    if x == 0:
+        count_zero += 1
+    else:
+        count_one += 1
+if count_one > count_zero:
+    print(count_zero)
+else:
+    print(count_one)
